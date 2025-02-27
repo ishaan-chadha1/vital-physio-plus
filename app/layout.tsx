8f3cb3376792eca1bea1ca6ac09b7190eead7fbc
Ishaan Chadha <ishaan.chadha12@gmail.com>
2025-02-27 17:51:57 +0530

    Change back to link

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <h2 href={"/"}>Welcome to Vital Physio +</h2>
+                    <Link href={"/"}>Welcome to Vital Physio +</Link>
 
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
