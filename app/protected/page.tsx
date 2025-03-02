15a3090a7434ea9980c7b4b5c6c3e4e68d074dc4
Ishaan Chadha <ishaan.chadha12@gmail.com>
2025-03-02 10:45:41 +0530

    Addition of Gemini to Protected Page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -28,6 +28,9 @@ export default async function ProtectedPage() {
       <Link href="/elevenlabs" className="text-blue-500 underline">
         Access ElevenLabs AI
       </Link>
+      <Link href="/chat" className="text-blue-500 underline">
+        Access Gemini
+      </Link>
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
