ebcdf68546de7924d114913b7f6577f0fb05c031
Ishaan Chadha <ishaan.chadha12@gmail.com>
2025-04-11 14:55:35 +0530

    Gemini Down from 2.0 - 1.5

--- config/Gemini.js
+++ config/Gemini.js
@@ -4,7 +4,7 @@ import {
 	HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const MODEL_NAME = "gemini-2.0-flash"; 
+const MODEL_NAME = "gemini-1.5-flash"; 
 console.log("✅ ENV Vars Available:", process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
 const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
